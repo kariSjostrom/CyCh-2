623d949df74f311bc5c55402dfaee366caa56e9a
kariSjostrom <s0472138@student.rockvalleycollege.edu>
2015-12-05 21:59:21 -0600

    Initial Commit

---
+++ CyCh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CyCh
+//
+//  Created by Karissa the Squid on 12/5/15.
+//  Copyright © 2015 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
