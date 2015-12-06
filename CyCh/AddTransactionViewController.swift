c7f7721e1147fda36519094204fab3199b735f78
kariSjostrom <s0472138@student.rockvalleycollege.edu>
2015-12-06 11:24:08 -0600

    -KAS

---
+++ CyCh/AddTransactionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddTransactionViewController.swift
+//  CyCh
+//
+//  Created by Karissa the Squid on 12/6/15.
+//  Copyright © 2015 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+
+class AddTransactionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
